394d64cc45c81908beddfad0f9cf39ad16aca679
gjuarez-ago <112523082+gjuarez-ago@users.noreply.github.com>
2023-10-29 20:48:22 -0600

    Avances del proyecto

--- hazloo/src/main/java/com/service/hazloo/repository/ILogRepository.java
+++ hazloo/src/main/java/com/service/hazloo/repository/ILogRepository.java
@@ -6,11 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.service.hazloo.domain.Notification;
+import com.service.hazloo.domain.Log;
 
-public interface ILogRepository extends JpaRepository<Notification, Long>{
-
-    @Query(value = "SELECT * FROM tbl_notificaciones WHERE id = :id",nativeQuery = true)
-	List<Notification> getNotificationById(@Param("id") Long plnId); 
+public interface ILogRepository extends JpaRepository<Log, Long> {
     
+    @Query(value = "SELECT * FROM tbl_log WHERE user_id = :user",nativeQuery = true)
+	List<Log> getLogByUser(@Param("user") Long userId); 
+    
+    @Query(value = "SELECT * FROM tbl_log WHERE id = :id",nativeQuery = true)
+    Log getLogById(@Param("id") Long id);
+
 }
--- hazloo/src/main/java/com/service/hazloo/repository/ITaskRepository.java
+++ hazloo/src/main/java/com/service/hazloo/repository/ITaskRepository.java
@@ -1,9 +1,17 @@
 package com.service.hazloo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.service.hazloo.domain.Log;
 import com.service.hazloo.domain.Task;
 
 public interface ITaskRepository extends JpaRepository<Task, Long>{
     
+    @Query(value = "SELECT * FROM tbl_tareas WHERE user_id = :user",nativeQuery = true)
+	List<Log> getTaskByUser(@Param("user") Long userId); 
+
 }
